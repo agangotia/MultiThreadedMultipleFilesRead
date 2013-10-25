180b06405da3cb44d1e954d50c0fc2595edf8392
Anupam Gangotia <agangotia@gmail.com>
2013-10-25 14:44:06 -0400

    v1

---
+++ MultiThreadedMultipleFilesRead/src/com/github/multithreadmulfileread/FileRead.java
@@ -0,0 +1,42 @@
+package com.github.multithreadmulfileread;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead implements Runnable {
+
+	private String threadName;
+	BufferedReader inputStream;
+
+	FileRead(String thrdName, String fileNameWPath)
+			throws FileNotFoundException {
+		this.threadName = thrdName;
+		inputStream = new BufferedReader(new FileReader(fileNameWPath));
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			String l;
+			while ((l = inputStream.readLine()) != null) {
+				System.out.println(threadName + "::" + l);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
---
+++ MultiThreadedMultipleFilesRead/src/com/github/multithreadmulfileread/FileReadMain.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.github.multithreadmulfileread;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Anupam Gangotia
+ *
+ */
+public class FileReadMain {
+	
+	
+	ArrayList<String> pathsNNamesOfFilesToRead=new ArrayList<String>();
+	private static final int NTHREDS = 5;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileReadMain objMain=new FileReadMain();
+		objMain.initializeFileNames();
+		ExecutorService executor = Executors.newFixedThreadPool(NTHREDS);
+	    for (int i = 0; i < 10; i++) {
+	      Runnable worker;
+		try {
+			worker = new FileRead("thread_"+i,objMain.pathsNNamesOfFilesToRead.get(i));
+			executor.execute(worker);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      
+	    }
+	    
+	    // This will make the executor accept no new threads
+	    // and finish all existing threads in the queue
+	    executor.shutdown();
+	    // Wait until all threads are finish
+	    while (!executor.isTerminated()) {
+        }
+	    System.out.println("Finished all threads");
+
+	}
+	
+	public void initializeFileNames(){
+		for(int i=0;i<10;i++){
+			String Path="C:\\Users\\anugan\\git\\MultiThreadedMultipleFilesRead\\MultiThreadedMultipleFilesRead\\TextFiles\\";
+			String FileName="file_"+i+".txt";
+			this.pathsNNamesOfFilesToRead.add(Path+FileName);
+			
+		}
+	} 
+
+}
